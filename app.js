e579c59466b7e47c5428d210ba9739a426be37e0
edwardoou <67031155+edwardoou@users.noreply.github.com>
2022-06-06 17:23:37 -0500

    files uploaded

---
+++ app.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const AWS = require('aws-sdk');
+require('dotenv').config();
+
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
+
+const uploadFile = (fileName) => {
+  const fileContent = fs.readFileSync(fileName);
+  const params = {
+    Bucket: "productos-bucket",
+    Key: fileName,
+    Body: fileContent,
+  };
+  // Uploading files to the bucket
+  s3.upload(params, function (err, data) {
+    if (err) {
+      throw err;
+    }
+    console.log(`File uploaded successfully. ${data.Location}`);
+  });
+};
+
+uploadFile("foto.png");
